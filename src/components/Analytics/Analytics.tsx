a6d105ec58e07a4855a8aaf7fe6499e5a440b71d
Sobayo <sobayotoby@gmail.com>
2023-07-08 21:52:26 +0100

    added analytics component

--- src/components/Analytics/Analytics.tsx
+++ src/components/Analytics/Analytics.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react"
+// import { useState } from "react"
 import LoadingGif from './../../assets/loadingGIF.gif'
 import classes from './analytics.module.css'
 
 const Analytics = () => {
-	const [loading, setLoading] = useState(true)
+	// const [loading, setLoading] = useState(true)
   return (
 	<div >
-	  {loading && <img src={LoadingGif}  className={classes.loading}/>}
+	  {/* {loading && <img src={LoadingGif}  className={classes.loading}/>} */}
+	  <img src={LoadingGif}  className={classes.loading}/>
 	</div>
   )
 }
